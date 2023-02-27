c5ae3885cdef1d178a6948b61453e8a677aa745e
Denis Vasimov <dvasimov@gmail.com>
2023-02-27 17:52:04 +0500

    Initial project with cmake

---
+++ build/CMakeFiles/CppServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppServer.dir/src/main.cpp.o"
+  "CMakeFiles/CppServer.dir/src/main.cpp.o.d"
+  "CppServer"
+  "CppServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CppServer.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CppServer.
+# This may be replaced when dependencies are built.
